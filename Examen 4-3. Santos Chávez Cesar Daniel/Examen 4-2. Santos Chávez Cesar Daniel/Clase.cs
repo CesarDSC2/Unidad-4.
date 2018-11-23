4735052954376a84b1f0eefe6056958412cd6c6c
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-11-23 13:45:29 -0800

    Add files via upload

---
+++ Examen 4-3. Santos Chávez Cesar Daniel/Examen 4-2. Santos Chávez Cesar Daniel/Clase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_4_2.Santos_Chávez_Cesar_Daniel
+{
+    class Clase
+    {
+        public void Play()
+        {
+            Arbol raiz = new Arbol(); //Creamos un objeto para cada nodo.
+            raiz.dato = "a";
+            Arbol b = new Arbol();
+            b.dato = "b";
+            Arbol c = new Arbol();
+            c.dato = "c";
+            Arbol d = new Arbol();
+            d.dato = "d";
+            Arbol e = new Arbol();
+            e.dato = "e";
+            Arbol f = new Arbol();
+            f.dato = "f";
+            Arbol g = new Arbol();
+            g.dato = "g";
+            Arbol h = new Arbol();
+            h.dato = "h";
+
+            raiz.izq = b; //Asignamos los sub-árboles de cada lado.
+            raiz.der = e;
+
+            b.izq = c; //Asignamos los nodos.
+            b.der = d;
+
+            e.izq = f;
+
+            f.izq = g; 
+            f.der = h;            
+
+            Console.WriteLine("Impresión PreOrden:"); //Despliege
+            raiz.ImprimirPre(raiz);
+
+            Console.WriteLine("\n\nImpresión PostOrden:");
+            raiz.ImprimirPost(raiz);
+            Console.ReadKey();
+        }
+    }
+}
